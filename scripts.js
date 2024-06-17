e6db8d6a994d79740cbc8caccd5563a4a22649f7
Giuseppe Partipilo <gpartipilo726@gmail.com>
2024-06-17 09:37:19 -0400

    Create computer player framework
    Create Random generator and Computer choice

---
+++ scripts.js
@@ -0,0 +1,28 @@
+console.log("Hello");
+
+//generates random number for computer
+let getRandomCompChoice = () => {
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(3);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+
+//makes computer decision based on random number recieved from getRandomCompChoice
+let getComputerChoice = () => {
+  let choice = getRandomCompChoice()
+
+  if (choice === 1){
+    return "rock";
+  }
+  else if (choice === 2){
+    return "paper";
+  }
+  else {
+    return "scissors";
+  }
+
+}
+
+console.log(getComputerChoice());
+
